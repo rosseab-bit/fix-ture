f464b90f118727f1a120fc0f6343195e8dcfd247
roseab <roseab.developer@gmail.com>
2025-09-04 23:28:23 -0300

    agrego pages nuevas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,56 @@
+import CanchaBackground from "@/components/CanchaBackground";
 import "@/styles/globals.css";
+import styles from "@/styles/Home.module.css";
+import { hidenShowView, timeOutEjecute } from "@/utils/toolsFuncions";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [showMainView, setShowMainView] = useState<boolean>(false);
+  const [spinPelota, setSpinPelota] = useState<boolean>(false);
+  const router = useRouter();
+  const goToDashboard = () => {
+    router.push("/menu");
+  };
+  const handleMenuAction = () => {
+    /*hidenShowView('test show view', showMainView, setShowMainView)
+      setSpinPelota(true);*/
+      timeOutEjecute(() => setSpinPelota(false), 1);
+      goToDashboard();
+  };
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setSpinPelota(true); // ejemplo: reinicia el spinner
+    timeOutEjecute(() => setSpinPelota(false), 1);
+
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <div className={`${styles.container_main}`}>
+        <CanchaBackground />
+        <section className={`${styles.show_container_content}`}>
+          {/*<DefineContentView />*/}
+          <Component {...pageProps} />
+        </section>
+        <section className={`${styles.menu_bar_bottom}`}>
+          <span
+            className={`${styles.menu_pelota} ${
+              spinPelota ? `${styles.spinner}` : ""
+            }`}
+            onClick={() => handleMenuAction()}
+          >
+            ⚽
+          </span>
+        </section>
+      </div>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,13 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import CanchaBackground from "@/components/CanchaBackground";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+import DefineContentView from "@/components/DefineContentView";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 export default function Home() {
-  return (
-    <>
-      <div className={`${styles.container_main}`}>
-        <CanchaBackground />
-        <section className={`${styles.menu_bar_bottom}`}>
-        <span className={`${styles.menu_pelota}`}>⚽</span>
+  const router = useRouter();
 
-        </section>
-      </div>
-    </>
-  );
+  useEffect(() => {
+    router.replace("/ranking"); // redirige a /dashboard
+  }, [router]);
+
+  return null // o un loader
 }
